2f7be6ff25e6494a6156d4e6160ac42ae2eb26c1
Gagan <rollingrocky360@gmail.com>
2024-12-01 20:16:06 +0530

    2024 init

---
+++ 2024/go.mod
@@ -0,0 +1,3 @@
+module github.com/RollingRocky360/AdventOfCode/aoc-2024
+
+go 1.22.5
---
+++ 2024/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/RollingRocky360/AdventOfCode/aoc-2024/day1"
+
+func main() {
+	day1.Run()
+}
