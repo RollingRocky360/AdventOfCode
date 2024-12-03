1aa1c352d284d218ae5a682cd082c1fe1a0d1a09
Gagan <rollingrocky360@gmail.com>
2024-12-04 01:14:10 +0530

    add: day3

--- 2024/main.go
+++ 2024/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/RollingRocky360/AdventOfCode/aoc-2024/day2"
+	"github.com/RollingRocky360/AdventOfCode/aoc-2024/day3"
 )
 
 func main() {
-	day2.Run()
+	day3.Run()
 }
